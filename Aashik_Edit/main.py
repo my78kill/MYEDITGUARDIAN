866bde246699ee77922b0aa27a4839011b4f38ea
my78kill <aigirlfriend143you@gmail.com>
2026-03-24 21:40:54 +0530

    Update main.py

--- Aashik_Edit/main.py
+++ Aashik_Edit/main.py
@@ -4,7 +4,7 @@ import re
 import asyncio
 import time
 from random import choice
-from Edit import *
+from Aashik_Edit.Edit import *
 from telegram import Update, Bot
 from pyrogram import Client, filters
 from pyrogram.types import Message
