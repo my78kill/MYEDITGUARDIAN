6c9b2a414020da1f841c34c934e203dece288212
my78kill <aigirlfriend143you@gmail.com>
2026-03-24 22:12:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ import os
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 SUDO_ID = list(map(int, os.environ.get("SUDO_ID", "").split()))
+
 MONGO_URI = os.environ.get("MONGO_URI")
 DB_NAME = os.environ.get("DB_NAME")
+
 LOGGER = os.environ.get("LOGGER", "True") == "True"
-BOT_NAME = os.environ.get("BOT_NAME", "Edit Guardian")
-SUPPORT_ID = int(os.environ.get("SUPPORT_ID"))
+BOT_NAME = os.environ.get("BOT_NAME", "AnonXHelper")
+
+SUPPORT_ID = int(os.environ.get("SUPPORT_ID", "0"))
