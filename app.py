f7b9ee1afb6e431a1d6db91987840e8533d6e01b
my78kill <aigirlfriend143you@gmail.com>
2026-03-25 14:03:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 from flask import Flask
-import threading
-import Aashik_Edit.main as bot
 
-app_flask = Flask(__name__)
+app = Flask(__name__)
 
-@app_flask.route('/')
+@app.route("/")
 def home():
-    return "Bot is running 😈"
-
-def run_bot():
-    print("Bot starting...")
-    bot.app.start()   # ❗ NO .run()
-    print("Bot started")
-
-# start bot in background
-threading.Thread(target=run_bot, daemon=True).start()
+    return "Bot is alive 😈"
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    app_flask.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
